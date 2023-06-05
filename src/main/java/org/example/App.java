7d5c366cd8a5ae1e4386b32ad6f6453540917636
Ilya Zenchenko <ilya.zenchenko@icloud.com>
2023-06-05 13:31:44 +0300

    criteria api

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.example.model.Item;
 import org.example.model.Person;
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -20,11 +25,25 @@ public class App {
 
             Person person = session.get(Person.class, 1);
 
+            for (Object o : filtrateObjects("id", session, Person.class)) {
+                System.out.println(o);
+            }
+
             System.out.println(person.getItems());
             session.getTransaction().commit();
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static <T> List filtrateObjects(String field, Session session, Class<T> clazz) {
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = builder.createQuery(clazz);
+        Root<T> root = criteriaQuery.from(clazz);
+        criteriaQuery.select(root).orderBy(builder.asc(root.get(field)));
+        Query query = session.createQuery(criteriaQuery);
+        return query.getResultList();
     }
 }
